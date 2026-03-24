f035b1236f18b297320d76c55f7ea0e71b7e0327
reikodurr <mihailrylov536@gmail.com>
2026-03-24 11:53:06 +0700

    final

--- database.py
+++ database.py
@@ -4,10 +4,12 @@ def create_db():
     conn = sqlite3.connect("todo.db")
     cursor = conn.cursor()
 
-    sql = """CREATE TABLE IF NOT EXISTS Task (id INTEGER PRIMARY KEY AUTOINCREMENT, text VARCHAR(1000) NOT NULL DEFAULT '',
-             is_done INTEGER DEFAULT  0
-             )
-    """
+    sql = '''CREATE TABLE IF NOT EXISTS task (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            text VARCHAR(1000) NOT NULL DEFAULT '',
+            is_done INTEGER DEFAULT  0
+        )
+    '''
 
     cursor.execute(sql)
     conn.commit()
@@ -16,16 +18,30 @@ def add_task(task_text):
     conn = sqlite3.connect("todo.db")
     cursor = conn.cursor()
 
-    cursor.execute("INSERT INTO Task (text) VALUES (?)", (task_text,))
+    cursor.execute("INSERT INTO task (text) VALUES (?)", (task_text,))
     conn.commit()
 
 def get_tasks():
     conn = sqlite3.connect("todo.db")
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM Task")
+    cursor.execute("SELECT * FROM task")
     tasks = cursor.fetchall()
     return tasks
+
+def delete_task(task_id):
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM task WHERE id = ?", (task_id,))
+    conn.commit()
+
+def change_task_status(task_id):
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+
+    cursor.execute("UPDATE task SET is_done = 1-is_done WHERE id = ?", (task_id,))
+    conn.commit()
 
 if __name__ == "__main__":
     create_db()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import database
 
 app = Flask(__name__)
@@ -7,6 +7,29 @@ app = Flask(__name__)
 def index():
     tasks= database.get_tasks()
     return render_template("index.html", tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task_text = request.form.get('task-text')
+    if task_text:
+        database.add_task(task_text)
+    return redirect(url_for('index'))
+
+@app.route("/delete", methods=['POST'])
+def delete():
+    task_id = request.form.get('task-id')
+
+    if task_id:
+        database.delete_task(task_id)
+    return redirect(url_for('index'))
+
+@app.route('/change_status', methods=['POST'])
+def change_status():
+    task_id = request.form.get('task-id')
+
+    if task_id:
+        database.change_task_status(task_id)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
