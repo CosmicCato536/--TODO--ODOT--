bc22e98cc6edc15a4c02e255731ee8e2184df4f5
okakSaikt <mihailrylov536@gmail.com>
2026-03-27 11:49:29 +0700

    updated database.py
    added user and admin with passwords (not full)

--- database.py
+++ database.py
@@ -3,15 +3,25 @@ import sqlite3
 def create_db():
     conn = sqlite3.connect("todo.db")
     cursor = conn.cursor()
+    sql = '''CREATE TABLE IF NOT EXISTS user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            login VARCHAR(1000) NOT NULL DEFAULT '',
+            password VARCHAR(1000) NOT NULL DEFAULT ''
+        )
+          '''
+    cursor.execute(sql)
 
     sql = '''CREATE TABLE IF NOT EXISTS task (
             id INTEGER PRIMARY KEY AUTOINCREMENT, 
             text VARCHAR(1000) NOT NULL DEFAULT '',
-            is_done INTEGER DEFAULT  0
+            is_done INTEGER DEFAULT  0,
+            user_id INTEGER NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES user(id)
         )
           '''
-
     cursor.execute(sql)
+
+
     conn.commit()
 
 def add_task(task_text):
@@ -43,11 +53,25 @@ def change_task_status(task_id):
     cursor.execute("UPDATE task SET is_done = 1-is_done WHERE id = ?", (task_id,))
     conn.commit()
 
+def add_user(login, password):
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO user (login, password) VALUES (?, ?)", (login, password))
+    conn.commit()
+
+def get_users():
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM user")
+    users = cursor.fetchall()
+    return users
+
 if __name__ == "__main__":
     create_db()
-    add_task("task 1")
-    add_task("task 2")
+    add_user("admin", "SAIKT")
+    add_user("user", "password")
 
-    tasks = get_tasks()
-    for task in tasks:
-        print(task)
+    users = get_users()
+    print(users)
